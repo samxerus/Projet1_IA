1662e1086670250d4c31a70d20626ff4b49d8ab7
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2026-03-13 17:56:49 +0100

    feat: add 3 comments diplay

--- atom_placement/main.py
+++ atom_placement/main.py
@@ -6,13 +6,24 @@ from search import *
 #       Launch      #
 #####################
 if __name__ == '__main__':
-    problem = AtomPlacement("instances/i01.txt")
+    problem = AtomPlacement("instances/i10.txt")
     init_state = problem.init_state()
     step_limit = 100
+
     node = random_walk(problem, step_limit)
-    # node = maxvalue(problem, step_limit)
-    # node = randomized_maxvalue(problem, step_limit)
-    print("Best solution found:")
+    print("Best solution found (random_walk):")
+    print(f"Objective: {node.value()}")
+    print(f"State: {node.state}")
+    print(f"Steps: {node.step}")
+
+    node = max_value(problem, step_limit)
+    print("Best solution found (maxvalue):")
+    print(f"Objective: {node.value()}")
+    print(f"State: {node.state}")
+    print(f"Steps: {node.step}")
+
+    node = randomized_max_value(problem, step_limit)
+    print("Best solution found (randomized_maxvalue):")
     print(f"Objective: {node.value()}")
     print(f"State: {node.state}")
     print(f"Steps: {node.step}")
